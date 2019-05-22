96559f348937b30c54297c7cc225dcf0fd52dded
Dita Aji Pratama <dita.aji.pratama@gmail.com>
2019-05-22 20:21:31 +0700

    Installation with new python version (3.6)

--- installation/install-linux/install-modules.sh
+++ installation/install-linux/install-modules.sh
@@ -1,6 +1,5 @@
-apt-get install python-pip
-
-pip install mysql-connector
-pip install cherrypy
-
-pip install mako
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+sudo python3.6 get-pip.py
+sudo pip3.6 install cherrypy
+sudo pip3.6 install mako
+sudo pip3.6 install mysql-connector
